c5baecd8e62a68a421af460fbf4588727796ffe2
ntholi <ntholi@live.com>
2019-03-28 16:18:07 +0200

    Add more components in main ui

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import luct.gradebook.GradebookTable;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	
+	BorderPane root = new BorderPane();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			BorderPane borderPane = new BorderPane();
-			borderPane.setCenter(new GradebookTable());
-			scene.setRoot(borderPane);
+			Scene scene = new Scene(root,600,500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			root.setLeft(createNav());
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -25,6 +29,25 @@ public class Main extends Application {
 		}
 	}
 	
+	private VBox createNav() {
+		Button gradesBtn = new Button("Grades");
+		gradesBtn.setMinWidth(100);
+		gradesBtn.setMinHeight(100);
+		Button studentBtn = new Button("Students");
+		studentBtn.setMinWidth(100);
+		studentBtn.setMinHeight(100);
+		
+		gradesBtn.setOnAction(e ->{
+			root.setCenter(new HBox());
+		});
+		GradebookTable students = new GradebookTable();
+		studentBtn.setOnAction(e ->{
+			root.setCenter(students);
+		});
+		
+		return new VBox(gradesBtn, studentBtn);
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
